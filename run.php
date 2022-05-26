5f57d23b99c31d04eb205087d1b461211c38ea27
hisune <hi@hisune.com>
2022-05-26 16:26:06 +0800

    update readme

--- run.php
+++ run.php
@@ -1,2 +1,9 @@
 <?php
+$parseUrl = parse_url('127.0.0.1');
+if(!isset($parseUrl['scheme'])) $parseUrl['scheme'] = 'http';
+if(!isset($parseUrl['port'])) $parseUrl['port'] = '8096';
+$url = $parseUrl['scheme'] . '://' . ($parseUrl['host'] ?? $parseUrl['path']) . ':' . $parseUrl['port'];
+var_dump($url);
+
+exit;
 require_once __DIR__ . '/app.phar';
